b4d71b1cfe86aae90925c09a4a20867a54c6f99e
Keshav Kashyap <109139023+Kashyap-1225@users.noreply.github.com>
2022-10-30 21:55:09 +0530

    Update p3.cpp

--- p3.cpp
+++ p3.cpp
@@ -1,28 +1,24 @@
-   /* 
-        123456
-        123456
-        123456
-        123456
- */
- 
- #include<iostream>
- using namespace std;
- int main(){
-    int a,b;
-    cin>>a;
- 
-    int i=1;
-    while(i<=a){
-        int j=1;
-        while(j<=a){
-            cout<< j;
-            j+=1;
-        
-            
-
-        }
+/*                        1
+                2       3       2
+        3       4       5       4       3
+4       5       6       7       6       5       4
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k;
+    cout<<"Enter the no. of lines required to print\t";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+            cout<<"\t";
+        for(k=i;k<(2*i-1);k++)
+            cout<<k<<"\t";
+        for(int m=k;m>=i;m--)
+            cout<<m<<"\t";
         cout<<endl;
-        i+=1;
-
     }
- }
+    return 0;
+}
